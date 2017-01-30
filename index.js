b584c49691b38c31ccc8f51a35c22081dc574f5b
Bobby Schaetzle <bobalu@panterasbox.com>
2017-01-30 05:17:40 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import expressWs from 'express-ws'
+
+var app = express();
+expressWs(app);
+ 
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+ 
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.end();
+});
+ 
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});
+ 
+app.listen(8080);
